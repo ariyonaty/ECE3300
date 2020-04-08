2b8e01a10e0bb4ddd62246dd9bfe5dc62d8aed69
ariyonaty <ari.h.yonaty@gmail.com>
2020-04-07 17:25:07 -0700

    Does not work as expected..

---
+++ Divider/Divider.srcs/sim_1/new/divider_tb.v
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/07/2020 05:13:11 PM
+// Design Name: 
+// Module Name: divider_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module divider_tb();
+
+    reg clk, reset, command;
+    reg [7:0] dataA;
+    reg [3:0] dataB;
+    wire done;
+    wire [7:0] quotient, remainder;
+    wire [2:0] i;
+    wire [15:0] registerRA;
+    wire [1:0] state_reg;
+
+    divider uut(clk, reset, command, dataA, dataB, quotient, remainder, done, i, registerRA, state_reg);
+
+    always #1 clk = ~clk;
+
+    initial begin
+            reset = 1; clk = 0; command = 0;
+        #1  dataA = 140; dataB = 9; 
+        #5  command = 1;
+    end
+
+endmodule
---
+++ Divider/Divider.srcs/sources_1/new/divider.v
@@ -0,0 +1,129 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/07/2020 04:25:19 PM
+// Design Name: 
+// Module Name: divider
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module divider
+    #(  parameter   dividendWidth = 8,
+        parameter   divisorWidth = 4
+    )(
+        input       clk,
+        input       reset,
+        input       command, 
+        input       [dividendWidth-1:0] dataA,
+        input       [divisorWidth-1:0] dataB,
+        output reg  [dividendWidth-1:0] quotient,
+        output reg  [dividendWidth-1:0] remainder,
+        output reg  done,
+        output reg  [2:0] i,
+        output reg  [2*(dividendWidth-1):0] registerRA,
+        output reg  [1:0] state_reg
+    );
+
+    localparam [1:0]    S0 = 0,
+                        S1 = 1,
+                        S2 = 2,
+                        S3 = 3;
+
+    reg [1:0] state_next;
+    reg [dividendWidth-1:0] registerA, registerA_next;
+    reg [dividendWidth-1:0] registerB, registerB_next;
+    reg [2*(dividendWidth-1):0] registerRA_next;
+    reg [dividendWidth-1:0] quotient_next;
+    reg [dividendWidth-1:0] remainder_next;
+    reg [2:0] i_next;
+    reg done_next;
+
+
+    always @(posedge clk) begin
+        if (reset) begin
+            state_reg       <= S0;
+            registerA       <= 0;
+            registerB       <= 0;
+            registerRA      <= 0;
+            quotient        <= 0;
+            remainder       <= 0;
+            i               <= dividendWidth - 1;
+            done <= 0;
+        end else begin
+            state_reg       <= state_next;
+            registerA       <= registerA_next;
+            registerB       <= registerB_next;
+            registerRA      <= registerRA_next;
+            quotient        <= quotient_next;
+            remainder       <= remainder_next;
+            i               <= i_next;
+            done            <= done_next;
+        end
+    end
+
+    always @(*) begin
+        state_next          <= state_reg;
+        registerA_next      <= registerA;  
+        registerB_next      <= registerB; 
+        registerRA_next     <= registerRA;
+        quotient_next       <= quotient;
+        remainder_next      <= remainder;  
+        i_next              <= i;
+        done_next           <= done;       
+    
+        case (state_reg)
+            S0: begin
+                    if (command) begin
+                        state_next = S1;
+                        done_next = 0;
+                    end else begin
+                        registerA_next = dataA;
+                        registerB_next = {4'b0, dataB};
+                        registerRA_next = {remainder, dataA};
+                        remainder_next = 0;
+                        i_next = dividendWidth - 1;
+                        done_next = 0;
+                    end
+                end
+            S1: begin
+                    registerRA_next = registerRA << 1;
+                    state_next = S2;
+                end
+            S2: begin
+                    if (registerRA[2*(dividendWidth - 1):dividendWidth] >= registerB) begin
+                        quotient_next[i] = 1;
+                        registerRA_next[2*(dividendWidth - 1):dividendWidth] = registerRA[2*(dividendWidth - 1):dividendWidth] - registerB;
+                    end
+                end
+            S3: begin
+                    done_next = 1;
+                    remainder_next - registerRA[2*(dividendWidth - 1):dividendWidth];
+                    if (!command) begin
+                        
+                    end
+                end
+            default: begin
+                    state_next = S0;
+                    done_next = 0;
+                end 
+        endcase
+
+    end
+
+    
+
+
+endmodule
